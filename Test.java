d6de491e0c2c9cb3ca368593868261182eb92155
jaduakram <washim.akram@taaffeite.co>
2024-09-09 23:20:36 +0530

    First Commit in java

---
+++ Test.java
@@ -0,0 +1,13 @@
+class Test
+{
+	public static void main(String[] args) {
+		System.out.println("Test Class");
+	}
+}
+
+class Test2{
+	public static void main(String[] args) {
+	System.out.println("Test2");
+		
+	}
+}
